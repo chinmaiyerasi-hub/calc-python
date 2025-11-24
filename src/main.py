7768e6ea21685a50947f4116bebfdeb0c98e38fe
chinmaiyerasi-hub <chinmaiyerasi@gmail.com>
2025-11-24 10:20:29 +0530

    Initialize Flask app for food blog

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🍽️ My Food Blog</h1>
+    <p>Welcome to my simple food blog!</p>
+
+    <h2>Recipes</h2>
+    <ul>
+        <li>Margherita Pizza</li>
+        <li>Chocolate Cake</li>
+        <li>Pasta Alfredo</li>
+    </ul>
+    """
+
+if __name
